1ee308d74741b5f9a1e99bcc384c970b9bbc19be
الكنج حمودي الاسطوره <mogsnfknsgqns@gmail.com>
2024-10-10 15:38:25 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+cd ~
+cd HasTek
+mkdir wordlist
+apt install tar -y
+apt install requests -y
+pkg install time -y
+pkg install re -y
+pkg install os -y
+pkg install webbrowser -y
+npm install -g bash-obfuscate
+chmod +x HasTek.sh
+
